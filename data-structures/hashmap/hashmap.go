34f545b9f6e023022bf3f7addd5a220d149ee8e4
adamy318 <ayu4@lsu.edu>
2019-04-07 19:21:05 -0500

    added handling of hash collisions

--- data-structures/hashmap/hashmap.go
+++ data-structures/hashmap/hashmap.go
@@ -4,19 +4,60 @@ import (
 //	"fmt"
 )
 
-const SIZE = 1000
+const SIZE = 10000000
 
 //type HashMap interface {
 //	InitHashTable() *HashTable
 //}
 
+type Key string
 
 type HashTable struct {
-	HashTable interface{}
+	HashTable [SIZE]KeyValue
+}
+
+type KeyValue struct {
+	key string
+	value string 
 }
 
 func InitHashTable() *HashTable  {
-	var array [SIZE]string
-	hash := &HashTable{array} 
-	return hash
+	var hash HashTable
+	return &hash
+}
+
+func (s Key) hashCode() int {
+	hash := 13
+	return hash * int(s[0]) * int(s[len(s)-1]) * 17 + len(s)
+}
+
+func (ht *HashTable) Put(key, value string) {
+	keyMap := Key(key)
+	hash := keyMap.hashCode()
+	for ht.HashTable[hash].key != "" {
+		hash++
+	}
+	ht.HashTable[hash].key = key
+	ht.HashTable[hash].value = value
+}
+
+func (ht *HashTable) Get(key string) string {
+	keyMap := Key(key)
+	hash := keyMap.hashCode()
+	for ht.HashTable[hash].key != key {
+		hash++
+	}
+	return ht.HashTable[hash].value
+}
+
+func (ht *HashTable) Delete(key string) string {
+	keyMap := Key(key)
+	hash := keyMap.hashCode()
+	for ht.HashTable[hash].key != key {
+		hash++
+	}
+	value := ht.HashTable[hash].value
+	ht.HashTable[hash].key = ""
+	ht.HashTable[hash].value = ""
+	return value
 }
--- data-structures/main.go
+++ data-structures/main.go
@@ -7,5 +7,18 @@ import (
 
 func main() {
 	nw := hashmap.InitHashTable()
-	fmt.Println(nw.HashTable)
+
+	nw.Put("test", "code")
+	nw.Put("go", "yes")
+	nw.Put("fox", "bear")
+	nw.Put("fish", "crab")
+	nw.Put("boom", "no")
+	nw.Put("brrm", "dkfjdf")
+
+	fmt.Println("Expected -> test: ", nw.Get("test"))
+	fmt.Println("Expected -> go: ", nw.Get("go"))
+	fmt.Println("Expected -> fox: ", nw.Get("fox"))
+	fmt.Println("Expected -> fish: ", nw.Get("fish"))
+	fmt.Println("Expected -> boom: ", nw.Get("boom"))
+	fmt.Println("Expected -> brrm: ", nw.Get("brrm"))
 }
